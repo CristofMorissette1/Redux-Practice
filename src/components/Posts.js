b329648d54c80310aff057c474daf94f5e228529
Cristof Morissette <cristofmorissette@Cristofs-MacBook-Air.local>
2019-07-30 14:40:42 -0600

    redux boilerplater

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,18 +2,6 @@ import React, { Component } from 'react'
 
 
 class Posts extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            posts: []
-        }
-    }
-
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(data => this.setState({posts: data}))
-    }
 
 
     render(){
