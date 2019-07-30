061f159995dd9b904f15c261efb14c58858dc385
Cristof Morissette <cristofmorissette@Cristofs-MacBook-Air.local>
2019-07-30 12:39:10 -0600

    setting up redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import Posts from './components/Posts.js'
-import './App.css'
-import PostForm from './components/PostsForm.js'
+import Posts from './components/Posts.js';
+import './App.css';
+import PostForm from './components/PostsForm.js';
+import { Provider } from 'react-redux'; 
+import Store from './store'
+
+
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <PostForm />
       <hr></hr>
       <Posts />
     </div>
+    </Provider>
   );
 }
 
