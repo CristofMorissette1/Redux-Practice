f474a1894000aaab0d879f2f9a913d52e8ee576f
Cristof Morissette <cristofmorissette@Cristofs-MacBook-Air.local>
2019-07-30 15:12:22 -0600

    redux boilerplater complete

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts} from '../actions/postActions'
+import PropTypes from 'prop-types'
 
 
 class Posts extends Component {
-
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
 
     render(){
-        const fetchedPosts = this.state.posts.map(post => 
+        const fetchPosts = this.props.posts.map(post => 
         <div key={post.id}>
             <h3>{post.title}</h3>
             <h3>{post.body}</h3>
         </div>)
         return(
             <div>
-                {fetchedPosts}
+                {fetchPosts}
             </div>
         )
     }
 }
 
-export default Posts;
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    posts: state.posts.items
+})
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
